c7639c5c16098b7c722de29acdd368a637e9fc72
José <marix_pp@hotmail.com>
2026-04-28 07:22:27 -0600

    primera parte

---
+++ practica01/procesador_comando.py
@@ -0,0 +1,16 @@
+import datetime
+
+def obtener_saludo(nombre_bot):
+
+def procesar_comando_recordar(argumento):
+
+def calcular_uptime(hora_inicio):
+
+def mostrar_ayuda():
+
+def iniciar_agente():
+
+def main():
+
+if __name__ == "__main__":
+    main()
